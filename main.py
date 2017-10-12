3cd9d6fc47b945b3ff10837ac8e6ec2a559a4f02
Ramya Bellala <nsramya.bellala@gmail.com>
2017-10-12 18:04:52 -0500

    finished build-a-blog

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:code123@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 app.secret_key = 'y337kGcys&zp38'
+db = SQLAlchemy(app)
 
 class Blog(db.Model):
 
@@ -17,11 +18,11 @@ class Blog(db.Model):
         self.title = title 
         self.blog = blog
 
-@app.route('/newpost')
+@app.route('/new_post')
 def index():
-    return render_template('newpost.html')
+    return render_template('new_post.html')
 
-@app.route('/newpost', methods=['POST', 'GET'])
+@app.route('/new_post', methods=['POST', 'GET'])
 def new_post():
     title_error =""
     blog_error = ""
@@ -43,7 +44,7 @@ def new_post():
 #            return str(input_id)
             return redirect('/blog?id='+str(input_id))
     else:
-        return render_template('/newpost.html',title_error=title_error,blog_error=blog_error)     
+        return render_template('/new_post.html',title_error=title_error,blog_error=blog_error)     
 
 @app.route('/blog')
 def post_blogs():
